0d2a2d4c9d5140f85dc010c7c4475b6f61df3179
Ljfernando <Ljfernando@dons.usfca.edu>
2017-09-21 15:47:15 -0700

    Changing redis port to 6379

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var express     = require('express')
   , redis       = require('redis')
   , redisClient
   , port        = process.argv[2] || 8003
-  , rport       = process.argv[3] || 6380
+  , rport       = process.argv[3] || 6379
   , debug       = process.argv[4] || null
 
 // Database setup
